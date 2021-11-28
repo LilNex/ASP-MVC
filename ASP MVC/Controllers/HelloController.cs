2edf5f4ad6fea2e9c65e5f2d763342f70fd70ddf
Ismail Chaiq <CHAIQ.ISMAIL@ofppt-edu.ma>
2021-11-28 11:27:15 +0100

    Hello World

---
+++ ASP MVC/Controllers/HelloController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASP_MVC.Models;
+
+namespace ASP_MVC.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        public ActionResult Index() {
+            Visiteur visiteur = new Visiteur();
+            return View(visiteur);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Index(Visiteur client)
+        {
+            Visiteur visiteur = new Visiteur();
+            string nom = "";
+            nom = Request.Form["nom_visiteur"];
+            visiteur.nom = nom;
+            ViewData["message"] = "Hello World " + visiteur.nom;
+            return View("Index", visiteur);
+        }
+    }
+}
---
+++ ASP MVC/Models/Visiteur.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP_MVC.Models
+{
+    public class Visiteur
+    {
+        public string nom { get; set; }
+    }
+}
